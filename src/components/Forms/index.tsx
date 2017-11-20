b7dafd1f2a72b2ed2d6e954d9cd62efea302f8d3
Joao Tavares <joaotavares@Joaos-MacBook-Air.local>
2017-11-20 12:39:56 -0300

    Finalizando a funcionalidade de mostrar os campos no form

--- src/components/Forms/index.tsx
+++ src/components/Forms/index.tsx
@@ -2,10 +2,19 @@ import * as React from 'react'
 import { withFormik, FormikProps } from 'formik'
 
 interface Fields {
-  newComentText: string;
+  nomePessoa: string;
   cidade: string;
   numeroDias: string;
   dataViagem: string;
+  sexoMFO: "m"|"f"|"o";
+  banhos: number;
+  saidasNoites: "sim" | "nao";
+  numeroSaidasNoite: number;
+  cidadePraia: "sim" | "nao";
+  praias: "sim" | "nao";
+  numeroPraia: number;
+  compromissos: "sim" | "nao";
+  numeroCompromissos: number;
 }
 
 const InnerForm = ({
@@ -19,19 +28,37 @@ const InnerForm = ({
     <form onSubmit={handleSubmit}>
       <p>Qual o seu nome?</p>
                 <input 
-                    name="newComentText"
-                    value= {values.newComentText}
+                    name="nomePessoa"
+                    value= {values.nomePessoa}
                     onChange={handleChange}
                     placeholder="Qual o seu nome?"
                 />
-                <p>Para onde vamos?</p>
+
+      <p>Qual o seu sexo</p>
+                <input 
+                type="radio"
+                name="sexoMFO"
+                onChange={handleChange}
+                value="m"/> Masculino
+               <input 
+                type="radio"
+                name="sexoMFO"
+                onChange={handleChange}
+                value="f"/> Feminino
+               <input 
+                type="radio"
+                name="sexoMFO"
+                onChange={handleChange}
+                value="o"/> Outro
+
+      <p>Para onde vamos?</p>
                 <input 
                     name="cidade"
                     value= {values.cidade}
                     onChange={handleChange}
                     placeholder="Para Onde Vamos"
                 />
-                <p>Quantos dia pretende passar lá?</p>
+      <p>Quantos dia pretende passar lá?</p>
                 <input 
                     name="numeroDias"
                     type="number"
@@ -39,14 +66,133 @@ const InnerForm = ({
                     onChange={handleChange}
                     placeholder="Numero de dias"
                 />
-                <p>Qual dia pretende viajar?</p>
+      <p>Qual dia pretende viajar?</p>
                 <input 
                     name="dataViagem"
+                    type="date"
                     value= {values.dataViagem}
                     onChange={handleChange}
-                    placeholder="DD/MM/AAAA"
+                    //placeholder="DD/MM/AAAA"
                 />
-                <button type ="submit">Comentar</button>
+       <p>Quantos Banhos você toma no dia? (Juro que isso será confidencial)</p>
+                <input 
+                name="banhos"
+                type="number"
+                onChange={handleChange}
+                value= {values.banhos}
+                /> 
+
+        <p>Vai ter algum compromissso lá em {values.cidade}?</p>
+                <input 
+                name="compromissos"
+                type="radio"
+                value="sim"
+                onChange={handleChange}
+                checked={values.compromissos === "sim"}/>Sim
+                <input 
+                name="compromissos"
+                type="radio"
+                value="nao"
+                onChange={handleChange}
+                checked={values.compromissos === "nao"}/>Não
+                {
+                  values.compromissos === "sim" ?(
+                    <div>
+                      <p>Quantos dias de compromissos você vai ter lá?</p>
+                      <input 
+                    name="compromissos"
+                    type="number"
+                    onChange={handleChange}
+                    value={values.numeroCompromissos}/> 
+                    </div>
+                  ):(
+                    <span></span>
+                  )
+                }  
+
+        <p>Prentende sair algum dia a noite?</p>
+                <input 
+                name="saidasNoites"
+                type="radio"
+                value="sim"
+                onChange={handleChange}
+                checked={values.saidasNoites === "sim"}/>Sim
+                <input 
+                name="saidasNoites"
+                type="radio"
+                value="nao"
+                onChange={handleChange}
+                checked={values.saidasNoites === "nao"}/>Não
+                {
+                  values.saidasNoites === "sim" ?(
+                    <div>
+                      <p>Quantos dias pretende sair?</p>
+                      <input 
+                    name="numeroSaidasNoite"
+                    type="number"
+                    onChange={handleChange}
+                    value={values.numeroSaidasNoite}/> 
+                    </div>
+                  ):(
+                    <span></span>
+                  )
+                }  
+        <p>Em {values.cidade} tem praia?</p> 
+        <input 
+                name="cidadePraia" 
+                value="sim"
+                onChange ={handleChange}
+                checked = {values.cidadePraia === "sim"}
+                type="radio"
+                />  Sim
+                <input 
+                name="cidadePraia" 
+                value="nao"
+                onChange ={handleChange}
+                checked = {values.cidadePraia === "nao"}
+                type="radio"
+                /> Não   
+  
+                    {
+                      values.cidadePraia === "sim"?(
+                        <div>
+                        <p>Vamos a La Playa?</p>
+                        <input 
+                        name="praias" 
+                        value="sim"
+                        onChange ={handleChange}
+                        checked = {values.praias === "sim"}
+                        type="radio"
+                        />  Sim
+                        <input 
+                        name="praias" 
+                        value="nao"
+                        onChange ={handleChange}
+                        checked = {values.praias === "nao"}
+                        type="radio"
+                        /> Não
+                        {
+                          values.praias === "sim" ?(
+                            <div>
+                              <p>Quantos dias vai a praia?</p>
+                              <input 
+                            name="numeroPraia"
+                            type="number"
+                            onChange={handleChange}
+                            value={values.numeroPraia}/> 
+                            </div>
+                          ):(
+                            <span></span>
+                          )
+                        }   
+                      </div>
+                      ):(
+                        <span></span>
+                      )
+                    }                  
+                        
+                    <br/>
+                <button type ="submit">Enviar</button>
     </form>
   );
 
@@ -55,7 +201,7 @@ const MyForm = withFormik({
     // Transform outer props into form values
     mapPropsToValues: props => ({ email: '',
     password: '',
-    newComentText:'',
+    nomePessoa:'',
     dataViagem: '',
     cidade: '',
     numeroDias:'',
@@ -78,8 +224,8 @@ const MyForm = withFormik({
   // Use <MyForm /> anywhere
   const Basic = () => (
     <div>
-      <h1>My Form</h1>
-      <p>This can be anywhere in your application</p>
+      <h1>Arrume Minha Mala</h1>
+      <h2>Seu assistente pessoal para lhe ajudar a arrumar a suas roupas</h2>
       <MyForm />
     </div>
   );
