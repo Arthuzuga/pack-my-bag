ae0fa4627359f63fbba72478836933783e9ceb81
Marcus Silva <mvfsillva@gmail.com>
2018-11-01 17:35:26 -0300

    fix remove comments

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,6 @@
-// var path = require("path");
-var webpack = require("webpack");
-var combineLoaders = require("webpack-combine-loaders");
-// var CopyWebpackPlugin = require("copy-webpack-plugin");
+
+const webpack = require("webpack");
+const combineLoaders = require("webpack-combine-loaders");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const WebappWebpackPlugin = require("webapp-webpack-plugin");
 const ImageminPlugin = require("imagemin-webpack-plugin").default;
@@ -15,20 +14,12 @@ const FAVICON_DIR = "./src/images/maleta.png";
 
 module.exports = {
     entry: ["./src/index.tsx"],
-    // mode: DEV_MODE ? "development" : "production",
     devtool: DEV_MODE ? "source-map" : "",
-    resolve: {
-        extensions: ['.js', '.ts', '.tsx', '.json']
-    },
     module: {
         loaders: [{
                 test: /\.tsx?$/,
                 loader: "babel-loader?presets[]=es2015,plugins[]=transform-runtime!awesome-typescript-loader"
             },
-            // {
-            //     test: /\.jsx?$/,
-            //     loader: "babel-loader?presets[]=es2015,plugins[]=transform-runtime"
-            // },
             {
                 test: /\.css/,
                 loader: combineLoaders([{
@@ -67,28 +58,27 @@ module.exports = {
                     }
                 ])
             },
-            {
-                test: /\.woff(2)?(\?v=[0-9].[0-9].[0-9])?$/,
-                loader: "url-loader?mimetype=application/font-woff"
-            },
-            {
-                test: /\.(ttf|eot)(\?v=[0-9].[0-9].[0-9])?$/,
-                loader: "file-loader?name=[name].[ext]"
-            },
+            // {
+            //     test: /\.woff(2)?(\?v=[0-9].[0-9].[0-9])?$/,
+            //     loader: "url-loader?mimetype=application/font-woff"
+            // },
+            // {
+            //     test: /\.(ttf|eot)(\?v=[0-9].[0-9].[0-9])?$/,
+            //     loader: "file-loader?name=[name].[ext]"
+            // },
             {
                 test: /\.(jpe?g|png|gif|svg)$/i,
                 loaders: [
                     'file-loader?hash=sha512&digest=hex&name=[hash].[ext]',
-                    // 'image-webpack?bypassOnDebug&optimizationLevel=7&interlaced=false'
                 ]
             }
         ]
     },
     resolve: {
-        extensions: [".tsx", ".ts", ".js", ".json"],
+        extensions: ['.js', ".tsx", ".ts", ".json"],
     },
     output: {
-        path: __dirname + "/dist",
+        path: `${__dirname}/dist`,
         publicPath: "/",
         filename: "bundle.[hash].js",
     },
@@ -104,15 +94,6 @@ module.exports = {
             "process.env.NODE_ENV": JSON.stringify(NODE_ENV),
             "process.env.PORT": JSON.stringify(PORT),
         }),
-        /*
-            Copy files from src to dist
-        */
-        // new CopyWebpackPlugin([
-        //     { from: "src/file.type" }
-        // ]),
-        /*
-            END
-        */
         new HtmlWebpackPlugin({
             template: "./src/index.html",
             filename: "index.html",
