d5b4ddfa5dacfcfe684b6c61e061c91c99886679
joao tavares <joao.arthur.tavares@gmail.com>
2023-04-21 14:41:49 -0300

    feat(Footer): create footer component

---
+++ src/app/components/Footer/footer.tsx
@@ -0,0 +1,61 @@
+import Link from "next/link";
+
+import {
+  RiLinkedinLine,
+  RiGithubLine,
+  RiInstagramLine,
+  RiTwitterLine,
+  RiBallPenLine,
+} from "react-icons/ri";
+
+import styles from "./footer.module.css";
+
+const Footer = () => {
+  return (
+    <footer className={styles.container}>
+      <div className={styles.name}>
+        <span>&#169; 2023 Todos os direitos reservados.</span>
+      </div>
+      <div className={styles.menu}>
+        <div className={styles.menuItem}>
+          <Link href="/blog">
+            <span className={styles.link}>Blog</span>
+            <RiBallPenLine className={styles.icon} />
+          </Link>
+        </div>
+        <div className={styles.menuItem}>
+          <Link
+            href="https://www.linkedin.com/in/joao-arthur-tavares/"
+            target="_blank"
+          >
+            <span className={styles.link}>LinkedIn</span>
+            <RiLinkedinLine className={styles.icon} />
+          </Link>
+        </div>
+        <div className={styles.menuItem}>
+          <Link href="https://github.com/arthuzuga" target="_blank">
+            <span className={styles.link}>Github</span>
+            <RiGithubLine className={styles.icon} />
+          </Link>
+        </div>
+        <div className={styles.menuItem}>
+          <Link
+            href="https://www.instagram.com/joaoarthurtavares/"
+            target="_blank"
+          >
+            <span className={styles.link}>Instagram</span>
+            <RiInstagramLine className={styles.icon} />
+          </Link>
+        </div>
+        <div className={styles.menuItem}>
+          <Link href="https://twitter.com/JoaoA_Tavares" target="_blank">
+            <span className={styles.link}>Twitter</span>
+            <RiTwitterLine className={styles.icon} />
+          </Link>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
